a0ab57b4843c5289ca08cefce99530496f8f64bc
littlewolf99 <wolf.home99@outlook.com>
2023-03-28 20:43:16 -0300

    #feature/01 - Implementação da estrutura inicial de components

---
+++ src/app/features/goodsreceipt/goodsreceipt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goodsreceipt',
+  templateUrl: './goodsreceipt.component.html',
+  styleUrls: ['./goodsreceipt.component.scss']
+})
+export class GoodsreceiptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
