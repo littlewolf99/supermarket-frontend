5348777445d29a68331780f43cbcd662673f380a
littlewolf99 <wolf.home99@outlook.com>
2023-04-15 18:25:29 -0300

    #feature/06 - Ajustes nos testes que falharam

--- src/app/features/account/register/register.component.spec.ts
+++ src/app/features/account/register/register.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { FormBuilder } from '@angular/forms';
+import { AccountService } from '../services/account.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -8,7 +11,8 @@ describe('RegisterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      providers: [FormBuilder, AccountService, HttpClient, HttpHandler]
     })
     .compileComponents();
 
