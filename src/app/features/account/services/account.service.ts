fd08b875dad526f10a82610a49e67ffbc5ee0da9
littlewolf99 <wolf.home99@outlook.com>
2023-04-15 20:53:10 -0300

    #feature/07 - Configuração e comunicação com o backend realizando criação de cadastro de usuário

--- src/app/features/account/services/account.service.ts
+++ src/app/features/account/services/account.service.ts
@@ -1,14 +1,22 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { UserData } from "../models/user-data";
+import { Observable } from "rxjs";
+import { catchError, map } from 'rxjs/operators'
+import { BaseService } from "src/app/services/base.service";
 
 @Injectable()
-export class AccountService { 
-    
-    constructor(private http: HttpClient) {}
+export class AccountService extends BaseService { 
 
-    registerUser(user: UserData) {
+    constructor(private http: HttpClient) { super(); }
 
+    registerUser(user: UserData): Observable<UserData> {
+        let response = this.http.post(this.UrlServiceV1 + 'user', user, this.GetHeaderJson())
+        .pipe(
+            map(this.extractData),
+            catchError(this.serviceError));
+
+        return response;
     }
 
     login(user: UserData) {
