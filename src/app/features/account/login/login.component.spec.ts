b6fbc4b6ad8a0439333c933bd3f2173e18e42a34
littlewolf99 <wolf.home99@outlook.com>
2023-05-01 10:25:34 -0300

    #feature/14 - Implementação de testes unitários para login

--- src/app/features/account/login/login.component.spec.ts
+++ src/app/features/account/login/login.component.spec.ts
@@ -12,10 +12,14 @@ import {
   USE_STORE 
 } from '@ngx-translate/core';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { FormGroup } from '@angular/forms';
+import { of, throwError } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let postSpy: jasmine.Spy;
+  let accountService: AccountService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -43,7 +47,72 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    accountService = TestBed.get(AccountService);
+    postSpy = spyOn(accountService, 'login');
+  });
+
+  const USER_TOKEN = {
+    login: 'test@teste.com',
+    token: 'abc123'
+  }
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should login successfully when login is correct', async() => {
+    const loginForm: FormGroup = component.loginForm;
+    loginForm.get('login').setValue('test@teste.com');
+    loginForm.get('password').setValue('123456');
+    loginForm.markAsDirty();
+    loginForm.valid;
+
+    postSpy.and.callFake((options: any) => {
+      expect(options.login).toEqual('test@teste.com');
+      expect(options.password).toEqual('123456');
+      return of(USER_TOKEN);
+    });
+    
+    const toastrService = TestBed.get(ToastrService);
+
+    const showSuccess = spyOn(toastrService, 'success').and.callFake((message: string, title: string) => {
+      expect(message).toBe('br_com_supermarket_LOGIN_SUCCESSFUL');
+    })
+
+    fixture.detectChanges();
+    component.login();
+
+    expect(postSpy).toHaveBeenCalled();
+    expect(showSuccess).toHaveBeenCalled();
+  });
+
+  it('should not login successfully when login is not correct', () => {
+    const loginForm: FormGroup = component.loginForm;
+    loginForm.get('login').setValue('test@teste.com');
+    loginForm.get('password').setValue('123456');
+    loginForm.markAsDirty();
+    loginForm.valid;
+
+    const errors = {
+      error: {
+        errors: [
+          'Teste'
+        ]
+      }
+    }
+
+    postSpy.and.returnValue(throwError(errors));
+
+    const toastrService = TestBed.get(ToastrService);
+
+    const showError = spyOn(toastrService, 'error').and.callFake((message: string, title: string) => {
+      expect(message).toBe('br_com_supermarket_LOGIN_AN_ERROR_OCCURRED_WHILE_LOGGING_IN');
+    })
+    component.login();
+    fixture.detectChanges();
+
+    expect(postSpy).toHaveBeenCalled();
+    expect(showError).toHaveBeenCalled();
   });
 });
